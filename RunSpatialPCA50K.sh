7ae4e0e28c7224c8dbfa9c0ead80aacccfc19502
mathiane <mathian.emilie@gmail.com>
2024-01-30 10:27:36 +0100

    modif bash script

--- RunSpatialPCA50K.sh
+++ RunSpatialPCA50K.sh
@@ -6,7 +6,7 @@
 #SBATCH --partition=high_p
 #SBATCH --account=gcs
 
-Rscript MultiPCARandomSampling50k.R --n_tiles 50000 \
+Rscript RunMultiSPCARandomSampling.R --n_tiles 50000 \
                                     --output_folder spatial_pca_test \
                                     --path2projectors /home/mathiane/LNENWork/LNEN_Molecular_groups_barlow_twin/projector/projector_concat_path_review.csv \
                                     --dimension_enc_vectors 128 
